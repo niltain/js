0419769c07c6b1ea62ffdde39dae1a16f49720d6
Dat Lyong <mrlinlyong@gmail.com>
2022-05-06 15:17:26 +0700

    thuchanhjs

---
+++ demo.js
@@ -0,0 +1,13 @@
+{
+
+    let a = prompt("giá trị a: ");
+    console.log(a);
+    let b= prompt("giá trị b: ");
+    console.log(b);
+    if ( a % b == 0){
+        alert("a là bội của b")
+    }
+    else{
+        alert("a kp là bội của b")
+    }
+}
